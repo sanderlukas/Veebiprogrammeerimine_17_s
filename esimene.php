8554804eb09748ccc7c537c546685895f6e47ba3
Sander Lukas <sanderlukas@users.noreply.github.com>
2017-09-12 14:15:45 +0300

    Lisatud on esimene PHP kood

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sander Lukas veebiprogrammeerimine </title>
+</head>
+<body>
+	<hl> Lukas veebiprogrammeerimine </hl>
+	<p> See leht on loodud õppetöö raames ning ei sisalda mingit tõsiseltvõetavat sisu. </p>
+	<p> SL - 21 - Rapla </p>
+	
+	<?php
+		echo "See on esimene jupp PHP väljastatud teksti!";
+	?>
+</body>
+</html>
