b052e2ed1b3f20ce1fd5e255dbf50611dcfeb87c
Sander Lukas <sanderlukas@users.noreply.github.com>
2017-09-19 14:31:20 +0300

    Lisatud on massiiv ja for-tsükkel

--- esimene.php
+++ esimene.php
@@ -3,9 +3,19 @@
 	$myName = "Sander";
 	$my_lastname = "Lukas";
 	
+	$monthNamesET = ["jaanuar", "veebruar", "märts",
+					 "aprill", "mai", "juuni", 
+					 "juuli", "august", "september", 
+					 "oktoober", "november", "detsember"];
+	
+	//var_dump($monthNamesET);
+	//echo $monthNamesET[8];
+	$monthNow = $monthNamesET[date("n") - 1];
+	
+	
 	//vaatame, mis kell on ja määrame päeva osa
 	$hourNow = date("H");
-	//echo $hourNow
+	//echo $hourNow;
 	$partOfDay = "";
 	
 	if ($hourNow < 8) {
@@ -17,14 +27,36 @@
 	if ($hourNow >= 16) {
 		$partOfDay = "vaba aeg";
 	}
-	//echo $partOfDay
+	//echo $partOfDay;
 	
 	//vaatame, kaua on koolipäeva lõpuni aega jäänud
 	$timeNow = strtotime(date("d.m.Y H:i:s"));
 	$schoolDayEnd = strtotime(date("d.m.Y" . " " . "15:45"));
-	$tillEnd = $schoolDayEnd - $timeNow
-	echo (round($tillEnd / 60));
+	$tillEnd = $schoolDayEnd - $timeNow;
+	//echo "Koolipäeva lõpuni jäänud " . round($tillEnd / 60) . " minutit";
 	
+	//Tegeleme vanusega 
+	$myBirthYear;
+	$ageNotice = "";
+	//var_dump($_POST);
+	if (isset($_POST["birthYear"])) {
+		$myBirthYear = $_POST["birthYear"]; 
+		$myAge = date("Y") - $_POST["birthYear"];
+		//echo $myAge;
+		$ageNotice = "<p> Teie vanus on ligikaudu " .$myAge ." aastat. </p>";
+		$ageNotice .= "<p> Olete elanud järgnevatel aastatel: </p>";
+		$ageNotice .= "\n <ul> \n";
+		$yearNow = date("Y");
+		
+		for ($i = $myBirthYear; $i <= $yearNow; $i ++) {
+			$ageNotice .= "<li>" .$i ."</li> \n"; 
+		}
+		$ageNotice .= "</ul>";
+	}
+	//Teeme Tsükli
+	/*for ($i = 0; $i < 10; $i ++) {
+		echo "ha ";
+	}*/
 	
 	
 ?>
@@ -35,21 +67,37 @@
 	<meta charset="utf-8">
 	<title>Sander Lukas veebiprogrammeerimine </title>
 </head>
-<body>
-	<hl>
+
+<body style = "background-color:PaleGoldenRod;">
+	<h1>
 	<?php
 		echo $myName . " " . $my_lastname;
 	?>
-		veebiprogrammeerimine </hl>
+		veebiprogrammeerimine </h1>
 	<p> See leht on loodud õppetöö raames ning ei sisalda mingit tõsiseltvõetavat sisu. </p>
 	<p> SL - 21 - Rapla </p>
 	
 	<?php
 		echo "<p> See on esimene jupp PHP väljastatud teksti! </p>";
 		echo "<p> Täna on "; 
-		echo date("d.m.Y") . ", kell lehe avamisel oli " . date("H:i:s");
+		echo date("d. ") .$monthNow .date(" Y") .", kell lehe avamisel oli " . date("H:i:s");
 		echo ". Käes on " . $partOfDay  . ".</p>";
 	?>
 	
+	<hr>
+	<h2> Natuke aastaarvudest </h2>
+	
+	<form method = "POST">
+		<label> Teie sünniaasta: </label>
+		<input type = "number" name = "birthYear" id = "birthYear" min = "1900" max = "2017"
+		value = "<?php echo $myBirthYear; ?>">
+		<input type = "submit" name = "submitBirthYear" value = "Kinnita">
+	</form>
+	
+	<?php
+		if ($ageNotice != "") {
+			echo $ageNotice;
+		}
+	?>
 </body>
 </html>
